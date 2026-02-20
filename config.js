ef551dfdf8eb3cd4f96fd7131fec712d8699ca8d
DocentesBrown <docentxsbrown@gmail.com>
2026-02-20 09:38:44 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // (Opcional) Podés dejar esto así y configurar la URL desde la pantalla de ingreso.
 // Si querés fijarla en código, reemplazá el texto por tu URL que termina en /exec.
-window.TRAYECTORIAS_BACKEND_URL = "PEGAR_WEB_APP_URL_AQUI";
+window.TRAYECTORIAS_BACKEND_URL = "https://script.google.com/macros/s/AKfycbyjPkCHiDFPNAXFmJU-rVicr1ggjKueB7LyX-NOv9h7V8dqVX30g8G-vEkI7Ur49TuMRA/exec";
